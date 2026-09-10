dfdd041b51ae60710ff0d97c0ad09149d4cf54c2
Tom3546 <otomasz811@gmail.com>
2026-09-11 00:02:32 +0200

    feat: add retractable sidebar

--- src/components/ui/SideBarItem.tsx
+++ src/components/ui/SideBarItem.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { type LucideIcon } from "lucide-react";
+import { useContext } from "react";
+import { SideBarContext } from "../../layouts/SideBar";
 
 export interface SideBarItemProps {
   icon: LucideIcon;
@@ -14,18 +16,24 @@ export default function SideBarItem({
   to,
   end,
 }: SideBarItemProps) {
+  const expanded = useContext(SideBarContext);
+
   return (
     <NavLink
       to={to}
       end={end}
       className={({ isActive }) =>
-        `relative flex items-center w-full p-3 pl-4 gap-4 rounded-md hover:cursor-pointer 
-        hover:bg-bg-light transition-all duration-200
-      ${isActive ? "text-primary" : "text-text"}`
+        `relative flex items-center p-3 rounded-md hover:cursor-pointer 
+        transition-all duration-200
+        ${isActive ? "text-primary bg-primary/15" : "text-text hover:bg-bg-light"}`
       }
     >
       <Icon className="w-6" />
-      <span>{label}</span>
+      <span
+        className={`overflow-hidden transition-all duration-800 ease-in-out ${expanded ? "w-56 ml-4" : "w-0 opacity-0"}`}
+      >
+        {label}
+      </span>
     </NavLink>
   );
 }
--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,9 +1,10 @@
+import { createContext, useState } from "react";
 import { Link } from "react-router-dom";
+
 import logoDark from "../assets/Union.svg";
 import SideBarItem from "../components/ui/SideBarItem";
 
 import { ChevronsLeft } from "lucide-react";
-/* import { ChevronsRight } from "lucide-react"; */
 
 import { LayoutDashboard } from "lucide-react";
 import { ChartLine } from "lucide-react";
@@ -15,35 +16,54 @@ function HorizontalRule() {
   return <hr className="w-full mt-3 mb-3 border-[hsl(0,0%,15%)]" />;
 }
 
+export const SideBarContext = createContext(true);
+
 export default function SideBar() {
+  const [expanded, setExpanded] = useState(true);
+
   return (
     <aside
-      className="sticky flex flex-col items-center h-screen w-64 p-4 gap-1 border-r
+      className="sticky flex flex-col h-screen p-3 gap-1 border-r
       border-[hsl(0,0%,15%)] bg-bg"
     >
-      <div className="flex justify-between w-full mt-2 mb-8 pl-4 pr-2">
+      <div className="flex items-center justify-between w-full mt-2 mb-8 relative">
         <Link to="/">
-          <img src={logoDark} alt="logo" className="w-12 h-12" />
+          <img
+            src={logoDark}
+            alt="logo"
+            className={`absolute top-0 left-4 h-10 transition-all  
+              ${
+                expanded
+                  ? "delay-300 duration-500"
+                  : "pointer-events-none opacity-0 duration-400 -translate-x-4"
+              }`}
+          />
         </Link>
-
         <button
-          className="flex items-center justify-center h-10 w-10 p-2 rounded-full  bg-bg-light 
+          onClick={() => setExpanded((curr) => !curr)}
+          className="flex items-center justify-center h-10 w-10 mr-1 rounded-full  bg-bg-light 
           hover:cursor-pointer hover:brightness-110 transition-all duration-200"
         >
-          <ChevronsLeft />
+          <ChevronsLeft
+            className={`w-5 h-5 transition-transform duration-300 ${
+              expanded ? "rotate-0" : "rotate-180"
+            }`}
+          />
         </button>
       </div>
 
-      <SideBarItem icon={LayoutDashboard} label="Dashboard" to="/" end />
-      <HorizontalRule />
-      <SideBarItem icon={ChartLine} label="Performance" to="/performance" />
-      <SideBarItem icon={ChartPie} label="Holdings" to="/holdings" />
-      <SideBarItem
-        icon={ArrowLeftRight}
-        label="Transactions"
-        to="/transactions"
-      />
-      <SideBarItem icon={HandCoins} label="Dividends" to="/dividends" />
+      <SideBarContext value={expanded}>
+        <SideBarItem icon={LayoutDashboard} label="Dashboard" to="/" end />
+        <HorizontalRule />
+        <SideBarItem icon={ChartLine} label="Performance" to="/performance" />
+        <SideBarItem icon={ChartPie} label="Holdings" to="/holdings" />
+        <SideBarItem
+          icon={ArrowLeftRight}
+          label="Transactions"
+          to="/transactions"
+        />
+        <SideBarItem icon={HandCoins} label="Dividends" to="/dividends" />
+      </SideBarContext>
     </aside>
   );
 }
