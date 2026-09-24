f0d62b4758d575916f40caad8d703e06bbf5de18
Tom3546 <otomasz811@gmail.com>
2026-09-24 23:57:37 +0200

    feat: add recharts instead of lightweight charts for summarizing line chart

--- src/pages/DashBoard/DashBoardCharts.tsx
+++ src/pages/DashBoard/DashBoardCharts.tsx
@@ -1,41 +1,70 @@
-import { createChart, ColorType, AreaSeries } from "lightweight-charts";
-import { useEffect, useRef } from "react";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
 import { mockHistory } from "../../data/mockData";
-import { getColor } from "../../utils/colors";
 
 function LineChartComponent() {
-  const lineChartRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!lineChartRef.current) return;
-
-    const bgColor = getColor("--bg");
-    const lineColor = getColor("--primary");
-    const textColor = getColor("--text");
-    const areaTopColor = getColor("--primary");
-
-    const lineChart = createChart(lineChartRef.current, {
-      layout: {
-        background: { type: ColorType.Solid, color: bgColor },
-        textColor,
-      },
-      width: lineChartRef.current.clientWidth,
-      height: 300,
-    });
-    lineChart.timeScale().fitContent();
-
-    const newSeries = lineChart.addSeries(AreaSeries, {
-      lineColor,
-      topColor: areaTopColor,
-    });
-
-    newSeries.setData(mockHistory);
-
-    return () => {
-      lineChart.remove();
-    };
-  }, []);
-  return <div ref={lineChartRef}></div>;
+  return (
+    <div className="p-4 w-full h-80 bg-bg ring ring-border">
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart data={mockHistory}>
+          <defs>
+            {/* 1. Gradient pod wykresem (pionowy: góra -> dół) */}
+            <linearGradient id="underLineGradient" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="15%"
+                stopColor="var(--primary)"
+                stopOpacity={0.35}
+              />
+              <stop offset="95%" stopColor="var(--primary)" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid
+            vertical={false}
+            strokeDasharray="10 5"
+            stroke="var(--border)"
+          />
+          <XAxis
+            dataKey="time"
+            stroke="var(--text-muted)"
+            fontSize={12}
+            minTickGap={25}
+            padding={{ left: 1, right: 1 }}
+            tickFormatter={(value) => {
+              const [, month, day] = value.split("-");
+              return `${day}.${month}`;
+            }}
+          />
+          <YAxis
+            stroke="var(--text-muted)"
+            fontSize={12}
+            width={"auto"}
+            domain={["auto", "auto"]}
+            tickFormatter={(value) =>
+              value ? `${(value / 1000).toFixed(0)}k` : value
+            }
+            tickLine={false}
+            axisLine={false}
+          />
+          <Area
+            type="monotone"
+            dataKey="value"
+            stroke="var(--primary)"
+            fill="url(#underLineGradient)"
+            activeDot={{ stroke: "" }}
+            strokeWidth={2}
+            dot={false}
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
 
 export default function DashBoardCharts() {
--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,16 +1,8 @@
 import { rgb, formatRgb } from "culori";
 
-export function getRgbColoraaa(initialColor: string) {
-  const rgbObject = rgb(initialColor);
-  const rgbColor = formatRgb(rgbObject);
-
-  const styles = getComputedStyle(document.documentElement);
-  return styles.getPropertyValue(rgbColor ?? "#000000");
-}
-
 export function getColor(varName: string): string {
   const styles = getComputedStyle(document.documentElement);
-  const oklchColor = styles.getPropertyValue(varName).trim();
+  const oklchColor = styles.getPropertyValue(`--${varName}`).trim();
 
   const rgbObject = rgb(oklchColor);
   const rgbColor = formatRgb(rgbObject);
